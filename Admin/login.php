51ad263b3a3ca8d87d761e0b608e67e343496c80
Matthew Cline <r.matthew.cline@gmail.com>
2016-05-05 13:26:46 -0400

    added a login page to the admin area

---
+++ Admin/login.php
@@ -0,0 +1,22 @@
+<form action="" method="post">
+<p><label>Username</label><input type="text" name="username" value="" /></p>
+<p><label>Password</label><input type="text" name="password" value="" /></p>
+<p><label</label><input type"submit" name="submit" value="Login" /></p>
+
+<?php
+	if(isset($_POST['submit'])){
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+
+		if($user->login($usernam, $password)){
+			header('Location: index.php');
+			exit;
+		}
+		else
+		{
+			$message = '<p class=:error">Wrong username or password</p>';
+		}
+	}
+
+	if (isset($message)){echo $message;}
+?>
