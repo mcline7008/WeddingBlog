b6e02b3a6b7aa57fe1339885a612bcecb7c7fcc8
Matthew Cline <r.matthew.cline@gmail.com>
2016-05-06 11:51:53 -0400

    Fixed the login button bug on the login page

--- Admin/login.php
+++ Admin/login.php
@@ -1,7 +1,12 @@
+<?php
+	require_once('../includes/config.php');
+	if(!$user->is_loggid_in()){header('Location: login.php');}
+?>
+
 <form action="" method="post">
 <p><label>Username</label><input type="text" name="username" value="" /></p>
 <p><label>Password</label><input type="text" name="password" value="" /></p>
-<p><label</label><input type"submit" name="submit" value="Login" /></p>
+<p><label</label><input type="submit" name="submit" value="Login" /></p>
 
 <?php
 	if(isset($_POST['submit'])){
